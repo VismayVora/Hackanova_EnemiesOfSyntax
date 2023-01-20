179de4730ad416178aaf62259fca8ca8777bd44e
dishantzaveri <zaveridishant@gmail.com>
2023-01-20 15:26:39 +0530

    place details

--- App.js
+++ App.js
@@ -1,11 +1,62 @@
 import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Home from './components/Home';
+import Details from './components/Details';
+import Liked from './components/Liked';
+import Profile from './components/Profile';
+import colors from './assets/colors/colors';
 import { Place } from './screens';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Tabs from './navigation/tabs';
 
 const Stack = createStackNavigator();
+
+// Entypo.loadFont();
+// MaterialCommunityIcons.loadFont();
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+	return (
+		<Tab.Navigator
+			tabBarOptions={{
+				style: styles.tabBar,
+				activeTintColor: colors.orange,
+				inactiveTintColor: colors.gray,
+				showLabel: false,
+			}}>
+			<Tab.Screen
+				name="Home"
+				component={Home}
+				// options={{
+				// 	tabBarIcon: ({ color }) => (
+				// 		<Entypo name="home" size={32} color={color} />
+				// 	),
+				// }}
+			/>
+			<Tab.Screen
+				name="Liked"
+				component={Liked}
+				// options={{
+				// 	tabBarIcon: ({ color }) => (
+				// 		<Entypo name="heart" size={32} color={color} />
+				// 	),
+				// }}
+			/>
+			<Tab.Screen
+				name="Profile"
+				component={Profile}
+				// options={{
+				// 	tabBarIcon: ({ color }) => (
+				// 		<MaterialCommunityIcons name="account" size={32} color={color} />
+				// 	),
+				// }}
+			/>
+		</Tab.Navigator>
+	);
+};
 
 const App = () => {
 	return (
@@ -14,13 +65,32 @@ const App = () => {
 				screenOptions={{
 					headerShown: false,
 				}}
-				initialRouteName={'Dashboard'}>
+				initialRouteName={'TabNavigator'}>
 				<Stack.Screen name='Dashboard' component={Tabs} />
 
 				<Stack.Screen name='Place' component={Place} />
+				<Stack.Screen
+					name="TabNavigator"
+					component={TabNavigator}
+					options={{ headerShown: false }}
+				/>
+				<Stack.Screen
+					name="Details"
+					component={Details}
+					options={{ headerShown: false }}
+				/>
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
 };
+
+const styles = StyleSheet.create({
+	tabBar: {
+		backgroundColor: colors.white,
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
+	},
+});
+
 
 export default App;
