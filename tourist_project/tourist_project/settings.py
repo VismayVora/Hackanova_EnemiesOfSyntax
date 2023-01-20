92052233c5922662032b232f04d0d3d2c0af6e52
Vismay Vora <voravismay9@gmail.com>
2023-01-20 22:39:12 +0530

    Updated settings

--- tourist_project/tourist_project/settings.py
+++ tourist_project/tourist_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
     'corsheaders',
     'drf_spectacular',
     'phonenumber_field',
